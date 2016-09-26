7b6fd3eb5199e8046f604b919090ef22375e9679
Moritz Port <mport@hm.edu>
2016-09-26 21:50:34 +0000

    Capistrano init

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage, :production
+server '212.47.228.8', user: 'deploy', roles: %w{web app}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
